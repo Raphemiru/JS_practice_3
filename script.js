5c3a99e3f8ef04fcabcfa526dc9b8c4664c1d74f
Gab <fga.080272@gmail.com>
2026-06-03 23:12:11 +0800

    Quiz Screen Implement

---
+++ script.js
@@ -0,0 +1,13 @@
+// SCREENS
+const startScreen = document.getElementById('start-screen');
+const quizScreen = document.getElementById('quiz-screen');
+
+// startScreen assets
+const StartButton = document.getElementById('start-button');
+
+// quizScreen assets
+const quizQuestion = document.getElementById('quiz-question');
+const currentQuestion = document.getElementById('current-question');
+const totalQuestions = document.getElementById('total-questions');
+const currentScore = document.getElementById('current-score');
+const progressBar = document.getElementById('progress-bar');
